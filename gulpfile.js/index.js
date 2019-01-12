a6fd4ad8b40619d2289c26750c410f1b0efc0f4b
Martin Homola <adder10@gmail.com>
2019-01-12 22:12:17 +0100

    compiling twig templates

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,6 +1,31 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const { series, parallel, src, dest } = require('gulp');
+const del = require('del');
+const twig = require('gulp-twig');
+
+const dirs = {
+    src: './src',
+    dest: './dev'
+};
+
+function removeFiles() {
+    return del( dirs.dest).then( paths => {
+        console.log('Deleted files and folders:\n', paths.join('\n'));
+    });
 }
-    
-exports.default = defaultTask;
+
+function templates() {
+    return src(`${ dirs.src }/*.twig`)
+        .pipe( twig({
+            data: {
+                title: 'Gulp and Twig',
+                benefits: [
+                    'Fast',
+                    'Flexible',
+                    'Secure'
+                ]
+            }
+        }))
+        .pipe( dest( dirs.dest ) );
+    }
+
+exports.default = series( removeFiles, templates);
