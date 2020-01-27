cee3cdc5dc4dbed6a77de4ac8fdb5fdc529125ff
Martin Homola <adder10@gmail.com>
2020-01-27 15:00:55 +0100

    add swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,11 @@
 import $ from 'cash-dom';
 import svg4everybody from 'svg4everybody';
 // import inpageScroll from './components/inpageScroll';
+import initSwiper from './components/swiper';
+
 svg4everybody();
 
 $(document).ready(() => {
   // inpageScroll();
+  initSwiper();
 });
