3237ff69e53c01e991ad9feeade1d40452fa3dab
Martin Homola <adder10@gmail.com>
2020-04-14 18:43:44 +0200

    add swiper

--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,6 @@ const $ = window.$;
   window.svg4everybody()
 }());
 
-// main
-(function () {
-  const test = () => 'ahoj světe'
-  console.log(test())
-}());
-
 // gallery
 (function () {
   const wrapClass = '.js-gallery'
@@ -45,12 +39,14 @@ const $ = window.$;
     const options = {
       selector: wrapClass,
       closeButton: false,
-      skin: 'popup',
-      /*onOpen: () => {
+      skin: 'popup'
+      /*
+      onOpen: () => {
         $('.js-popup-close').on('click', () => {
           .close()
         })
-      }*/
+      }
+      */
     }
 
     return window.GLightbox({ ...options })
@@ -59,4 +55,89 @@ const $ = window.$;
   $wrap.each((index, item) => {
     createLightbox(popupClass.replace('.', ''))
   })
+}());
+
+// swiper
+(function () {
+  $('.swiper').not('.noinit').each((indexSw, elementSw) => {
+    const $swiper = $(elementSw)
+    const $container = $swiper.find('.swiper-container')
+    const $prev = $swiper.find('.swiper-button-prev')
+    const $next = $swiper.find('.swiper-button-next')
+    const data = $swiper.data()
+    const config = {}
+    let $pagination = $swiper.find('.swiper-pagination')
+    let i
+    let j
+    let count
+    let countParts
+
+    if ($swiper.hasClass('swiper-work')) {
+      $pagination = $swiper.parents('.reference-big').find('.swiper-pagination-work')
+    }
+
+    // add pagination
+    if ($pagination.length) {
+      config.pagination = {
+        el: $pagination[0],
+        clickable: true,
+        renderBullet: (index, className) => (`<span class="${className}"><span></span></span>`)
+      }
+    }
+
+    // prev&next button
+    if ($prev.length || $next.length) {
+      config.navigation = {
+        nextEl: $next,
+        prevEl: $prev
+      }
+    }
+
+    // slides per view
+    if (typeof data.count !== 'undefined') {
+      count = $swiper.data('count')
+
+      if (typeof count === 'string' && count.indexOf('|') !== -1) {
+        config.breakpoints = { }
+        countParts = count.split('|')
+
+        for (i = 0; i < countParts.length; i + 1) {
+          j = countParts[i].indexOf('-')
+          if (j !== -1) {
+            config.breakpoints[countParts[i].slice(0, j)] = {
+              slidesPerView: countParts[i].slice(j + 1)
+            }
+          } else {
+            config.slidesPerView = countParts[i].slice(j + 1)
+          }
+        }
+      } else {
+        config.slidesPerView = count
+      }
+
+      delete data.count
+    } else {
+      config.slidesPerView = 'auto'
+    }
+
+    // space between slides
+    if (typeof data.space !== 'undefined') {
+      config.spaceBetween = $swiper.data('space')
+      delete data.space
+    }
+
+    // put data atributes to config
+    $.extend(config, data)
+
+    // init
+    const sw = new window.Swiper($container, config)
+
+    return sw
+  })
+}());
+
+// main
+(function () {
+  const test = () => 'ahoj světe'
+  console.log(test())
 }())
